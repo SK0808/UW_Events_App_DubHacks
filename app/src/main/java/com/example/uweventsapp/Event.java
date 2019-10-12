c87f7de8870855df2e4e666d6d226d97c20b41e9
leonli33 <leonli3three@gmail.com>
2019-10-12 16:08:53 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/uweventsapp/Event.java
@@ -0,0 +1,42 @@
+package com.example.uweventsapp;
+
+public class Event {
+    private String Name;
+    private String Subject;
+    private String Date;
+    private String Location;
+    private String Time;
+    private String Description;
+
+    public Event(String name, String sub, String date, String loc, String time, String des) {
+        Name = name;
+        Subject = sub;
+        Date = date;
+        Location = loc;
+        Time = time;
+        Description = des;
+    }
+    public Event() {
+
+    }
+    public String getName() {
+        return Name;
+    }
+    public String getSubject() {
+        return Subject;
+    }
+    public String getDate()
+    {
+        return Date;
+    }
+    public String getLocation() {
+        return Location;
+    }
+    public String getTime() {
+        return Time;
+    }
+    public String getDescription() {
+        return Description;
+    }
+
+}
